0e885d54d5f50ca2f880adf7de47f05e58fd77da
GOKULTHEGREAT <gokul19072001@gmail.com>
2024-08-13 16:49:32 +0530

    sidenavbar and navbar fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,13 @@ import { AnalysisCardComponent } from './components/analysis-components/Analysis
 import { SidebarComponent } from './layout/sidebar/sidebar.component';
 import { DashboardcardComponent } from './components/dashboard-components/Dashboardcard/Dashboardcard.component';
 import { NavbarComponent } from './layout/navbar/navbar.component';
+import { DashboardComponent } from "./pages/dashboard/dashboard.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AnalysisCardComponent, DashboardcardComponent, SidebarComponent, NavbarComponent],
+  imports: [RouterOutlet, AnalysisCardComponent, DashboardcardComponent, SidebarComponent, NavbarComponent, DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
