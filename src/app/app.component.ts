24f6431e2e6fc7e5185f9a1df2729e10c08649ed
GOKULTHEGREAT <gokul19072001@gmail.com>
2024-08-13 12:14:14 +0530

    navbar, sidebar, dashboardcard, analysiscard created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AnalysisCardComponent } from './components/analysis-components/Analysiscard/Analysiscard.component';
+
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { DashboardcardComponent } from './components/dashboard-components/Dashboardcard/Dashboardcard.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AnalysisCardComponent, DashboardcardComponent, SidebarComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { GraphComponent } from "../../components/dashboard-components/graph/graph.component";
+import { DashboardcardComponent } from '../../components/dashboard-components/Dashboardcard/Dashboardcard.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [GraphComponent],
+  imports: [DashboardcardComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
