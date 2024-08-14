021bd52fd2887d2e7fb1e6848a203875f9da190c
Emma-Theresa <emmatheresajose12.4@gmail.com>
2024-08-14 15:55:53 +0530

    Changed style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,12 @@ import { AnalysisCardComponent } from './components/analysis-components/Analysis
 import { SidebarComponent } from './layout/sidebar/sidebar.component';
 import { DashboardcardComponent } from './components/dashboard-components/Dashboardcard/Dashboardcard.component';
 import { NavbarComponent } from './layout/navbar/navbar.component';
+import { DashboardComponent } from "./pages/dashboard/dashboard.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, SidebarComponent, DashboardcardComponent, AnalysisCardComponent, DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
