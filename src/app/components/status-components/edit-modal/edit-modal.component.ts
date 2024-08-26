e8f28dd9eab0e20ebca21108e8e9a12e5895e945
A-liya-s-alim <aliyasalim2001@gmail.com>
2024-08-26 12:12:07 +0530

    modal integrated with backend, VOC table styling edited

--- src/app/components/status-components/edit-modal/edit-modal.component.ts
+++ src/app/components/status-components/edit-modal/edit-modal.component.ts
@@ -1,5 +1,6 @@
 import { DashboardTable } from './../../../interface/dashboard-table';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
@@ -51,7 +52,7 @@ export class EditModalComponent {
     { field: 'feedbackStatus', header: 'Feedback Status', type: 'select', options: ['Received', 'Pending'] },
     { field: 'vocEligibilityDate', header: 'VOC Eligibility Date' },
   ];
-
+  constructor(private http: HttpClient) {}
   getEditableProjectField(field: string): any {
     return this.editableProject[field as keyof DashboardTable];
   }
@@ -66,16 +67,21 @@ export class EditModalComponent {
   }
 
   saveChanges() {
-    // Update the project with the new values
-  //   this.isModalOpen = false;
-  //   const projectIndex = this.projects.findIndex(
-  //     (proj) => proj.projectCode === this.editableProject.projectCode
-  //   );
-  //   if (projectIndex !== -1) {
-  //     this.editableProject[projectIndex] = { ...this.projects[projectIndex], ...this.editableProject };
-  //   }
+    // Make an HTTP PUT request to update the project in the backend
+    this.http.put(`https://localhost:7259/api/Project/editable/${this.editableProject.projectId}`, this.editableProject)
+      .subscribe(
+        () => {
+          // Handle success, e.g., close the modal, refresh the data, etc.
+          this.isModalOpen = false;
+          this.save.emit();
+          this.closeModal();
+        },
+        error => {
+          // Handle error, e.g., show an error message
+          console.error('Failed to save changes:', error);
+        }
+      );
 
-  // }
-  this.closeModal();
+    }
 }
-}
+
