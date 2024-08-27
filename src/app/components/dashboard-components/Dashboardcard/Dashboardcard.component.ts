ed08fe87e2192729fad382fd69d6f9c1cb12f879
Emma-Theresa <emmatheresajose12.4@gmail.com>
2024-08-27 09:33:49 +0530

    styling changed, loader added, excel template upload

--- src/app/components/dashboard-components/Dashboardcard/Dashboardcard.component.ts
+++ src/app/components/dashboard-components/Dashboardcard/Dashboardcard.component.ts
@@ -1,7 +1,8 @@
 import { NgForOf, NgIf } from '@angular/common'
-import { HttpClient } from '@angular/common/http';
+import { DashboardTableService } from '../../../service/dashboard-table.service';
+import { DashboardTable } from '../../../interface/dashboard-table';
 import { Component, OnInit } from '@angular/core';
-import { count } from 'rxjs';
+
 
 NgIf
 @Component({
@@ -12,70 +13,45 @@ NgIf
   styleUrl: './Dashboardcard.component.css'
 })
 export class DashboardcardComponent implements OnInit {
-  constructor(private http: HttpClient) { }
- 
-  
-  projects: any[] = [];
-  projectCodes: string[] = [];
-  projectNames: string[] = [];
-  dusAndDuHeads: string[] = [];
-  projectManager: string[] = [];
-  contractType: string[] = [];
-  numberOfResources: string [] = [];
-  customerName: string [] = [];
-  regions: string [] = [];
-  technologies: string [] = [];
-  status: string [] = [];
 
-  ongoingProjectsCount: number = 0;
-  fixedPriceCount: number = 0;
-  timeAndMoneyCount: number = 0;
-  isDivVisible: boolean = false;
-  cards: any[] = [];
+  cards: { numberText: string; cardText: string; checkboxes: boolean[] }[] = [];
+
+  constructor(private dashboardTableService: DashboardTableService) { }
+
+
 
   ngOnInit(): void {
-    this.fetchProjects();
+    this.getCardData();
   }
 
-  fetchProjects(): void {
-    this.http.get<any[]>('http://localhost:3000/projects').subscribe(data => {
-      this.projects = data;
-      this.projectCodes = [...new Set(data.map(project => project.ProjectCode))];
-      this.projectNames = [...new Set(data.map(project => project.ProjectName))];
-      this.dusAndDuHeads = [...new Set(data.map(project => project.DU + ' - ' + project.DUHead))];
-      this.projectManager = [...new Set(data.map(project => project.ProjectManager))];
-      this.contractType = [...new Set(data.map(project => project.ContractType))];
-      this.numberOfResources = [...new Set(data.map(project => project.NumberOfResources))];
-      this.customerName = [...new Set(data.map(project => project.CustomerName))];
-      this.regions = [...new Set(data.map(project => project.Region))];
-      this.technologies = [...new Set(data.map(project => project.Technology))];
-      this.status = [...new Set(data.map(project => project.Status))];
+    // Method to fetch and populate card data
+    getCardData() {
+      this.dashboardTableService.getProjects().subscribe((projects: DashboardTable[]) => {
 
-      // Calculate the number of ongoing projects
-      this.ongoingProjectsCount = data.filter(project => project.Status === 'Ongoing').length;
-      this.fixedPriceCount = data.filter(project => project.ContractType === 'Fixed Price').length;
-      this.timeAndMoneyCount = data.filter(project => project.ContractType === 'Time & Material').length;
+        const activeProjects=projects.filter(project=>project.status==='Ongoing').length;
 
-      // Initialize the cards array after fetching the data
-      this.cards = [
-        {
-          numberText: this.ongoingProjectsCount, 
-          cardText: this.status[0], 
-          
-        },
-        {
-          numberText: this.fixedPriceCount, 
-          cardText: this.contractType[0], 
-        },
-        {
-          numberText: this.timeAndMoneyCount,
-          cardText: this.contractType[1],
-        },
-      ];
-    });
-  }
-  onClick(i: any){
-     return this.isDivVisible=!this.isDivVisible;
-  }
+        const activeFPProjects=projects.filter(project=>project.status==='Ongoing'&& project.contractType==='Fixed Price').length;
 
+        const activeTMProjects=projects.filter(project=>project.status==='Ongoing'&& project.contractType==='Time & Material').length;
+        this.cards = [
+          {
+            numberText: activeProjects.toString(),
+            cardText: 'Active Projects',
+            checkboxes: Array(7).fill(false)
+          },
+          {
+            numberText: activeFPProjects.toString(),
+            cardText: 'Fixed Price Projects',
+            checkboxes: Array(7).fill(false)
+          },
+          {
+            numberText: activeTMProjects.toString(),
+            cardText: 'Time & Material Projects',
+            checkboxes: Array(7).fill(false)
+          }
+        ];
+      }, error => {
+        console.error('Error fetching projects:', error);
+      });
+    }
 }
