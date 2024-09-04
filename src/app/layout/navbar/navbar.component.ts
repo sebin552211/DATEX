d9d05fb80ffca01806e41013f3fae775f49d8ee9
Sebin Thomas <sebinthomas127@gmail.com>
2024-09-05 01:01:01 +0530

    resolves voc analysis page routing mistake, small styling changes in filter

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -12,7 +12,7 @@ import { RouterLink } from '@angular/router';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-  User='User';
+  User='ACE Team';
   items: MenuItem[] | undefined;
   ngOnInit() {
     this.items = [
